0df5151afcf48c30332508a29742e90b843a5aca
Julie Ann Villamor <villamorjulieann52@gmail.com>
2025-11-14 20:25:41 +0800

    Codes for Homepage

--- home.js
+++ home.js
@@ -1,22 +1,16 @@
-// Store the currently active element
 let activeElement = null;
-
-// Function to handle click event on shapes
 function toggleColor(element) {
     if (activeElement && activeElement !== element) {
-        activeElement.classList.remove("active"); // remove active from previous
+        activeElement.classList.remove("active");
     }
 
-    // Toggle active state
     if (activeElement !== element) {
         element.classList.add("active");
         activeElement = element;
     }
 }
 
-// Run after the page fully loads
 document.addEventListener("DOMContentLoaded", () => {
-    // Select your default shape here
     const defaultElement = document.querySelector(".rounded-upper");
 
     if (defaultElement) {
